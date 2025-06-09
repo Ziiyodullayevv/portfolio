5e8dedc37998a72c61e9b6f0eb5b7cbf61d10c8c
Ziiyodullayevv <akobirziyod2323@gmail.com>
2025-06-09 13:59:45 +0500

    bug fixed for images

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,16 +9,13 @@ export default function Hero() {
   return (
     <section className='min-h-screen relative overflow-hidden flex flex-col gap-10 justify-between items-center'>
       <div className='absolute top-0 left-0 right-0 bottom-0 -z-10 opacity-15 overflow-hidden flex justify-center items-center [filter:hue-rotate(50deg)]'>
-        {/* <img
-          className='w-full h-full object-cover'
-          src='/bg.png'
-          alt='background image'
-        /> */}
-        <ImageWithBlur
-          className='w-full h-full object-cover'
-          src={bg}
-          alt='bg image'
-        />
+        <SplitBox>
+          <ImageWithBlur
+            className='w-full h-full object-cover'
+            src={bg}
+            alt='bg image'
+          />
+        </SplitBox>
       </div>
 
       <div className='min-h-[50px] w-full'></div>
